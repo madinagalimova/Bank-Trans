ae88416b67556131af44b92e061688450063143e
madinagalimova <madinagalimova207@gmail.com>
2025-07-16 05:58:49 +0300

    Обновления: изменила имена и структуру транзакций

--- api/main.py
+++ api/main.py
@@ -9,6 +9,6 @@ def root():
 @app.get("/transactions")
 def get_transactions():
     return [
-        {"id": 1, "amount": 100.0, "sender": "МАДИНА", "receiver": "МИРОН"},
-        {"id": 2, "amount": 200.0, "sender": "ДМИТРИЙ", "receiver": "КИРИЛЛ"},
+        {"id": 1, "amount": 100.0, "from": "МАДИНА", "to": "МИРОН", "category": "еда", "date": "2025-07-15"},
+        {"id": 2, "amount": 200.0, "from": "ДМИТРИЙ", "to": "КИРИЛЛ", "category": "транспорт", "date": "2025-07-14"}
     ]
